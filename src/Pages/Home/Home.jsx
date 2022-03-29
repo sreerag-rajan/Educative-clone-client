276337523903dd9c9918168d4b0aef55ccf915fe
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-03-29 15:09:43 +0530

    some errors with redux fixed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import { Part1 } from "./Components/Part1"
+import { Part2 } from "./Components/Part2"
+import { Part3 } from "./Components/Part3"
+import { Part4 } from "./Components/Part4"
+
+export const Home = ()=>{
+    
+    return (
+        <div>
+            <Part1/>
+            <Part2/>
+            <Part3/>
+            <Part4/>
+        </div>
+    )
+}
--- src/Redux/Auth/auth.reducer.js
+++ src/Redux/Auth/auth.reducer.js
@@ -3,7 +3,7 @@ const initState = {
     user:null
 }
 
-export const authReducer = (store,{type,payload})=>{
+export const authReducer = (store = initState,{type,payload})=>{
     switch(type){
         default:
             return store;
