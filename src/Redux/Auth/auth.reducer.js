70329a00bb33b21c75f5105dba4e956a18bc911c
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-03-29 12:17:37 +0530

    added some folders that had got deleted

---
+++ src/Redux/Auth/auth.reducer.js
@@ -0,0 +1,12 @@
+
+const initState = {
+    user:null
+}
+
+export const authReducer = (store,{type,payload})=>{
+    switch(type){
+        default:
+            return store;
+    }
+
+}
---
+++ src/Redux/store.js
@@ -0,0 +1,15 @@
+import {combineReducers, compose, createStore} from "redux"
+import { authReducer } from "./Auth/auth.reducer"
+
+const rootReducer = combineReducers({
+    auth:authReducer
+})
+
+const loggerMiddleware = (store)=>(next)=>(action)=>{
+    if(typeof action === "function"){
+        return action(store.dispatch);
+    }
+    next(action);
+}
+
+export const store = createStore(rootReducer,compose(loggerMiddleware, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
