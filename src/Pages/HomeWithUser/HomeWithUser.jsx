187cc0c8ba62e4c946af530547743c0ffbf5abb2
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-03-30 20:08:47 +0530

    connected homeWithUser to backend

--- src/Pages/HomeWithUser/HomeWithUser.jsx
+++ src/Pages/HomeWithUser/HomeWithUser.jsx
@@ -1,12 +1,24 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
 import { ExploreMore } from "./Components/ExploreMore"
+import { FreeCourses } from "./Components/FreeCourses";
+import { PopularCourses } from "./Components/PopularCourses";
 import { Welcome } from "./Components/Welcome"
 
 
 export const HomeWithUser = ()=>{
+    const [courses,setCourses] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:2345/homecourses").then(({data})=>{
+            setCourses(data);
+        })
+    },[])
     return(
         <div>
             <Welcome/>
-
+            <FreeCourses courses={courses}/>
+            <PopularCourses courses={courses}/>
             <ExploreMore/>
         </div>
     )
