7509b168649bb1b78490da83ae5949645bd5b917
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-02 18:53:48 +0530

    Onboarding added

---
+++ src/Pages/Onboarding/Components/Onboard.jsx
@@ -0,0 +1,144 @@
+import  "./Onboard.css"
+
+export const Onboard =() =>{
+
+    return(
+        <div className="h3">
+            <div className="h5">
+                <p className="h1">Onboard your new software engineers <br />.Upskill the ones you already have.</p>
+                <p className="h2">Onboarding your growing team of developers is monotonous <br /> , difficult, and time-consuming. It doesn't have to be.</p>
+  
+                <button className="h4">Request a demo</button>
+                <br /><br /><br /><br />
+                
+                <img src="https://i.ibb.co/1LCvq2v/Screenshot-2022-03-29-213548.png" alt="" />
+               </div>
+                  {/* ist part is done   */}
+                <div className="h6">
+                    <button className="h7" onClick={()=>{
+                        
+                    }}>Onboard</button>
+                    <button className="h7">upskill</button>
+                    <button className="h7">retain</button>
+                </div>
+                {/* <div>this is the blank div for images button functionally</div> */}
+                <div>
+                    <img src="https://i.ibb.co/KxNQdbf/Screenshot-2022-04-02-184329.png" alt="" />
+                </div>
+
+                {/* third part  */}
+                <div className="h16">
+                    <div> <p className="h1">How we help you onboard your
+                    <br /> 
+                         teams more efficiently </p>
+                    </div>
+                     <div className="h17">
+                         <div>
+                            <img src="https://i.ibb.co/kQwK9Sf/Screenshot-2022-03-30-104431.png" alt="" />
+                         </div>
+                         <div className="h18">
+                             <div>
+                                 <p className="h1">Custom Onboarding Plans</p>
+                                 </div>
+
+                              <div>   <p className="h10">Engineering teams are like fingerprints - no two are exactly <br />  alike. Tailor unique paths for teams so new hires fill their  knowledge gaps and ramp on broader technology, your products, and internal processes. Include in your custom  plans things like:</p>
+                                 </div>
+                             <div className="h19">
+                             <div className="h11">
+                                   <img src="https://i.ibb.co/vzXGVn4/Screenshot-2022-03-30-150048.png" alt="" />
+                                   <p>Release process</p>
+                               </div>
+                               <div className="h11">
+                                   <img src="https://i.ibb.co/vzXGVn4/Screenshot-2022-03-30-150048.png" alt="" />
+                                   <p>Code libraries</p>
+                               </div>
+                               <div className="h11">
+                                   <img src="https://i.ibb.co/vzXGVn4/Screenshot-2022-03-30-150048.png" alt="" />
+                                   <p>Team Structure</p>
+                               </div>
+                             </div>
+                         </div>
+                     </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+             </div>     
+                     <div className="h20">
+                         <div className="h21">
+                          <p className="h1">Educative Courses Integration</p>
+                          <p className="h10">Need to teach a new language or technology? Want to knock the rust off an established skill? Integrate your onboarding with the leading provider of hands-on interactive coding so your software engineers grow faster and farther.</p>
+                         </div>
+                         <div>
+                             <img src="https://i.ibb.co/mz1RnXs/Screenshot-2022-03-30-165039.png" alt="" />
+                         </div>
+                     </div>
+
+              
+
+                {/* fourth image part is working from here */}
+                <div className="h22" >
+                    {/* first div for images and the second div for written part */}
+                    <div className="h23">
+                        <img className="h26" src="https://i.ibb.co/dsNw42c/Screenshot-2022-03-31-150317.png" alt="" />
+                    </div>
+
+                    <div className="h24">
+                        <p className="h25">Assign Mentors </p>
+                        <p className="h10"> New hires can quickly get overwhelmed during <br /> Onboarding and are unaware of whom to reach out to for <br /> help. Enhance their onboarding experience by assigning <br /> them Mentors              </p>
+                     
+
+                    </div>
+                </div>
+                {/* fifth part is working from here  time 15.39 */}
+                <div className="h22">
+                    {/* ist div for text and second  div for images */}
+                    <div className="24">
+                        <p className="h25">Unblock your mentees</p>
+                        <p className="h10">Jump in when help is needed. Stay out of the way when it’s <br /> not. Mentees can raise their virtual hand to signal their <br /> mentor when they’re stuck. Notifications alert the mentor <br /> upon login to answer the question and get the mentee <br /> back on track.</p>
+                    </div>
+                    <div className="h26">
+                        <img src="https://i.ibb.co/mF3pCwC/Screenshot-2022-03-31-153600.png" alt="" />
+                    </div>
+                </div>
+                <div className="h22">
+                    {/* this for sixth part first is image and second is text part  time 7:37 */}
+                    <div className="h23">
+                        <img src="https://i.ibb.co/xL3zmNp/Screenshot-2022-03-31-192513.png" alt="" />
+                    </div>
+                    <div className="h27">
+                        <p className="h25">Capture Actionable Insights</p>
+                        <p className="h10">Oversee the progress of your new hires, the performance of your mentors, and the overall efficiency of your Onboarding program, with our reports and dashboards.</p>
+                    </div>
+                </div>
+                <div className="h22">
+                    {/* seventh part first   div for the text and the second div for the  images7:44*/}
+                    <div className="h27">
+                        <p className="h25">White Glove Service</p>
+                        <p className="h10">Migrate your existing content and assets from anywhere to Onboarding or let Educative create a tailored Onboarding program from scratch just for you.</p>
+                    </div>
+                    <div className="h26">
+                        <img src="https://i.ibb.co/C6rjL7n/Screenshot-2022-03-31-192604.png" alt="" />
+                    </div>
+                </div>
+
+
+                <div className="h28">
+                {/* first div is for images and the second div for the  text content  */}
+                     <div >
+                    <img src="https://i.ibb.co/hHY4LQD/Screenshot-2022-04-01-074939.png" alt="" />
+                     </div>
+                    <div className="h29">
+                    <p className="h25">Ready for a demo?</p>
+                    <p className="h10">See Educative in action to learn how the power of developer experiences can push your team faster and farther than ever before.</p>
+                    <button className="h30">Request Demo</button>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+
